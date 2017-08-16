10a09e7b789bd61a562a4ccd815f9d1cc26b19b3
Jesse Barkdoll <jessebarkdoll@gmail.com>
2017-08-15 23:32:39 -0400

    change output format; update json data

--- bot.js
+++ bot.js
@@ -4,15 +4,15 @@ console.log('Bot started...');
 const Twit = require('twit');
 const TwitterBot = require('node-twitterbot').TwitterBot;
 
-const kotowaza = require('./kotowaza.json');
+const data = require('./kotowaza.json');
 
 // Setup for config variables in Heroku
-const Bot = new TwitterBot({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
+// const Bot = new TwitterBot({
+//     consumer_key: process.env.BOT_CONSUMER_KEY,
+//     consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//     access_token: process.env.BOT_ACCESS_TOKEN,
+//     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// });
 
 // The main funcion that returns a random proverb
 function getWisdom() {
@@ -22,21 +22,29 @@ function getWisdom() {
       return arr[randomNumber];
    }
 
-   let chosen = pickTweet(kotowaza);
-   let post = `${chosen[0]}\n${chosen[1]}`;
+   let chosen = pickTweet(data);
+
+   let kotowaza = `【${chosen[0]}】${chosen[1]}`;
+   let imi = chosen[2].split(/\n\n/)[1];
+
+   let post = `${kotowaza}\n${imi}`;
+
+   if (post.length > 140) {
+       post = `${post.substr(0, 137)}…`;
+   }
 
    return post;
 }
 
 function tweetIt() {
    let tweet = getWisdom()
-
+   console.log(tweet);
    // Uses node-twitterbot to send the data to twitter and post.
-   Bot.tweet(tweet);
+   // Bot.tweet(tweet);
 }
 
 // Executes once intially on startup.
 tweetIt();
 
 // Makes a post every hour.
-setInterval(tweetIt, 1000 * 60 * 60)
+// setInterval(tweetIt, 1000 * 60 * 60)
